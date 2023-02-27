f0ece9ec7f4e997128f666322f41e0bb0b9401c2
sabarish196 <sabarishreddy7@gmail.com>
2023-02-27 16:56:13 -0700

    Update final-with-nextlink.py

--- final-with-nextlink.py
+++ final-with-nextlink.py
@@ -2,54 +2,84 @@ import requests
 import json
 import os
 
-# Set the SPN credentials
-tenant_id = '<your tenant ID>'
-client_id = '<your client ID>'
-client_secret = '<your client secret>'
+# Set environment variables for SPN credentials
+tenant_id = os.environ["AZURE_TENANT_ID"]
+client_id = os.environ["AZURE_CLIENT_ID"]
+client_secret = os.environ["AZURE_CLIENT_SECRET"]
 
-# Set the Azure endpoint for recommendations API
-endpoint = 'https://management.azure.com/providers/Microsoft.Advisor/recommendations?api-version=2021-01-01'
+# Set the API endpoint
+api_endpoint = "https://management.azure.com/subscriptions?api-version=2020-01-01"
 
 # Disable SSL verification
 verify_ssl = False
 
-# Initialize headers with content type and authorization token
-headers = {'Content-Type': 'application/json'}
-auth_url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/token'
-auth_data = {
-    'grant_type': 'client_credentials',
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'resource': 'https://management.azure.com/'
+# Set the output file path
+output_file_path = "output.json"
+
+# Create an empty list to store recommendations
+all_recommendations = []
+
+# Set the headers
+headers = {
+    "Content-Type": "application/json",
 }
-auth_response = requests.post(auth_url, data=auth_data).json()
-access_token = auth_response['access_token']
-headers['Authorization'] = 'Bearer ' + access_token
 
-# Initialize an empty list to store recommendations
-recommendations = []
+# Authenticate using SPN credentials
+data = {
+    "grant_type": "client_credentials",
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "resource": "https://management.azure.com/",
+}
 
-# Send a GET request to the Azure endpoint to retrieve recommendations
-while endpoint:
-    response = requests.get(endpoint, headers=headers, verify=verify_ssl)
-    response_json = response.json()
-    if response_json.get('value'):
-        recommendations.extend(response_json['value'])
-    endpoint = response_json.get('nextLink')
+response = requests.post(
+    "https://login.microsoftonline.com/{}/oauth2/token".format(tenant_id),
+    data=data,
+    headers=headers,
+    verify=verify_ssl,
+)
 
-# Filter the recommendations to include only the desired fields
-filtered_recommendations = []
-for recommendation in recommendations:
-    filtered_recommendation = {
-        'id': recommendation['id'],
-        'type': recommendation['type'],
-        'name': recommendation['name'],
-        'extendedProperties': recommendation['properties']['extendedProperties'],
-        'resourceMetadata': recommendation['properties']['metadata']['resourceMetadata']
-    }
-    filtered_recommendations.append(filtered_recommendation)
+access_token = response.json()["access_token"]
+headers["Authorization"] = "Bearer " + access_token
 
-# Write the recommendations to a file in JSON format
-output_file = 'recommendations.json'
-with open(output_file, 'w') as f:
-    json.dump(filtered_recommendations, f, indent=4)
+# Loop through each subscription to get recommendations
+while api_endpoint:
+    # Get the subscriptions and convert response to JSON
+    response = requests.get(api_endpoint, headers=headers, verify=verify_ssl)
+    subscriptions = response.json()
+
+    # Loop through each subscription and get recommendations
+    for subscription in subscriptions["value"]:
+        # Set the API endpoint for recommendations
+        recommendations_api_endpoint = (
+            f"https://management.azure.com/subscriptions/{subscription['subscriptionId']}/providers/Microsoft.Advisor/recommendations?api-version=2017-04-19"
+        )
+
+        # Loop through each page of recommendations
+        next_link = recommendations_api_endpoint
+        while next_link:
+            # Get the recommendations and convert response to JSON
+            response = requests.get(next_link, headers=headers, verify=verify_ssl)
+            recommendations = response.json()
+
+            # Loop through each recommendation and get the extendedProperties and resourceMetadata fields
+            for recommendation in recommendations["value"]:
+                recommendation_properties = recommendation["properties"]
+                extended_properties = recommendation_properties.get("extendedProperties", {})
+                resource_metadata = recommendation_properties.get("resourceMetadata", {})
+                recommendation_dict = {
+                    "subscriptionId": subscription["subscriptionId"],
+                    "extendedProperties": extended_properties,
+                    "resourceMetadata": resource_metadata,
+                }
+                all_recommendations.append(recommendation_dict)
+
+            # Check if there is a next page of recommendations
+            next_link = recommendations.get("nextLink")
+
+    # Check if there are more subscriptions
+    api_endpoint = subscriptions.get("nextLink")
+
+# Write all recommendations to output file
+with open(output_file_path, "w") as f:
+    json.dump(all_recommendations, f)
