b9a87ffe75f2862f7af03b4a0172973079d07ab5
sabarish196 <sabarishreddy7@gmail.com>
2023-06-13 13:08:38 -0700

    Update getprefix.sh

--- getprefix.sh
+++ getprefix.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-ip_address="192.168.1.100"
-subnet_mask="255.255.255.0"
+ip_address=$1
+subnet_mask=$2
 
-IFS='.' read -r -a ip_octets <<< "$ip_address"
-IFS='.' read -r -a subnet_octets <<< "$subnet_mask"
-
-prefix=0
-for ((i=0; i<${#subnet_octets[@]}; i++)); do
-  subnet_octet=${subnet_octets[i]}
-  ip_octet=${ip_octets[i]}
-  
-  subnet_bin=$(printf "%08d" $(bc <<< "obase=2;$subnet_octet"))
-  ip_bin=$(printf "%08d" $(bc <<< "obase=2;$ip_octet"))
-  
-  for ((j=0; j<8; j++)); do
-    subnet_bit=${subnet_bin:j:1}
-    ip_bit=${ip_bin:j:1}
-    
-    if [[ $subnet_bit == "1" && $ip_bit == "1" ]]; then
-      prefix=$((prefix+1))
-    else
-      break
-    fi
-  done
+# Calculate the binary representation of the subnet mask
+IFS='.' read -ra mask_octets <<< "$subnet_mask"
+binary_mask=""
+for octet in "${mask_octets[@]}"; do
+  binary_octet=$(printf "%08d" $(echo "obase=2;$octet" | bc))
+  binary_mask="$binary_mask$binary_octet"
 done
 
-echo "$prefix"
+# Count the number of consecutive 1s in the binary subnet mask
+prefix=0
+for ((i = 0; i < ${#binary_mask}; i++)); do
+  if [[ ${binary_mask:i:1} == "1" ]]; then
+    ((prefix++))
+  else
+    break
+  fi
+done
+
+echo "Prefix: $prefix"
