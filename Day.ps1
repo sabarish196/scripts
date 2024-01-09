331867c278e116e3391984dbd2554837f1c9f017
sabarish196 <sabarishreddy7@gmail.com>
2024-01-09 15:00:35 -0700

    Update Day.ps1

--- Day.ps1
+++ Day.ps1
@@ -15,3 +15,21 @@ function ConvertToFullDayName($dayAbbreviation) {
 $dayAbbreviation = "fri"
 $fullDay = ConvertToFullDayName $dayAbbreviation
 Write-Host "Full Day: $fullDay"
+
+
+$currentDay = (Get-Date).DayOfWeek
+$currentHour = (Get-Date).Hour
+
+# Define your specific days and times
+$startDay = 'Thursday'
+$startTime = 18  # 6:00 PM
+$endDay = 'Tuesday'
+$endTime = 8    # 8:00 AM
+
+if (
+    ($currentDay -eq $startDay -and $currentHour -ge $startTime) -or
+    ($currentDay -eq $endDay -and $currentHour < $endTime) -or
+    ($currentDay -gt $startDay -and $currentDay -lt $endDay)
+) {
+    Write-Host "Yes"
+}
