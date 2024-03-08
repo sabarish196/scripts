024f147305741f82e14ad3b1a812954e3b2c294f
sabarish196 <sabarishreddy7@gmail.com>
2024-03-08 10:59:36 -0700

    Update Patch.sh

--- Patch.sh
+++ Patch.sh
@@ -7,3 +7,13 @@ curl -X PATCH \
      -H "Content-Type: application/vnd.api+json" \
      -d "{\"data\":{\"type\":\"workspaces\",\"attributes\":{\"terraform-version\":\"$NEW_TERRAFORM_VERSION\"}}}" \
      https://app.terraform.io/api/v2/workspaces/$WORKSPACE_ID
+
+
+
+export TOKEN=<your_api_token>
+
+curl -X GET \
+     -H "Authorization: Bearer $TOKEN" \
+     -H "Content-Type: application/vnd.api+json" \
+     "https://app.terraform.io/api/v2/organizations/<organization_name>/workspaces?page[size]=100" \
+     | jq '.data[] | select(.attributes.name | startswith("Azure-seia")) | .attributes.name'
